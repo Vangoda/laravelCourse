2514dd6da05cc321360c6e0a729ae4fbe75db5e9
Vangoda <vangoda@vangoda.com>
2022-05-24 18:05:33 +0000

    Added a route for login

--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Admin group
 Route::prefix('admin')->group(function () {
     // The route
     Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
 
 // Ambassador
