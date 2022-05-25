bc0b8dc13327c4d40a9e1590be96ba5f878d4a4e
Vangoda <vangoda@vangoda.com>
2022-05-25 17:12:43 +0000

    Moved logout to middleware group

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,10 @@ Route::prefix('admin')->group(function () {
     // The route
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('user', [AuthController::class, 'user']);
+        Route::post('logout', [AuthController::class, 'logout']);
     });
 });
 
