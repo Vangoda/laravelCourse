38092b59fdca3b257e183fea3ac9f7d9acdcb242
Vangoda <vangoda@vangoda.com>
2022-05-30 14:30:40 +0000

    Created API controller for Product Model

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Returns all products from the DB
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Create a new product model and store it in DB
+        $product = Product::create($request->only(
+            'title',
+            'description',
+            'imageURI',
+            'price'
+        ));
+
+        return response($product, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        // Return a single product by ID (laramagic)
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        // Update existing Product model and save changes
+        $product->update($request->only([
+            'title',
+            'description',
+            'imageURI',
+            'price'
+        ]));
+
+        return response($product, Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        // Deletes the existing product model and remove it from the DB
+        $product->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
