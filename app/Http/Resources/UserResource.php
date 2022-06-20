5e9378abc7aa1226c1611efc85b0076bfe1c4b11
Vangoda <vangoda@vangoda.com>
2022-06-20 10:36:04 +0000

    Created user resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            // Get revenue only for ambassador requests
+            $this->mergeWhen($request->path() === 'api/ambassador/user', [
+                'revenue' => $this->revenue
+            ])
+        ];
+    }
+}
