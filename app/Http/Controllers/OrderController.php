d23923fb3e04659b1c2e49085fe53ecdf0024160
Vangoda <vangoda@vangoda.com>
2022-06-25 18:34:13 +0000

    Implemented transactions to avoid corruption

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,6 +8,9 @@ use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class OrderController extends Controller
 {
@@ -39,36 +42,49 @@ class OrderController extends Controller
         // Get the link from code
         $link = Link::where('code', $request->input('code'))->first();
 
-        // Create new Order object
-        $order = new Order();
+        // Create new Order
+        // Using try catch block to stop DB transaction on any exception
+        try{
+            // Start a transaction to avoid data corruption.
+            DB::beginTransaction();
+            $order = new Order();
 
-        // Set order properties
-        $order->code = $link->code;
-        $order->user_id = $link->user_id;
-        $order->ambassador_email = $link->user->email;
-        $order->first_name = $request->input('firstName');
-        $order->last_name = $request->input('lastName');
-        $order->email = $request->input('email');
-        $order->address = $request->input('address');
-        $order->country = $request->input('country');
-        $order->city = $request->input('city');
-        $order->zip = $request->input('zip');
-        
-        $order->save();
+            // Set order properties
+            $order->code = $link->code;
+            $order->user_id = $link->user_id;
+            $order->ambassador_email = $link->user->email;
+            $order->first_name = $request->input('firstName');
+            $order->last_name = $request->input('lastName');
+            $order->email = $request->input('email');
+            $order->address = $request->input('address');
+            $order->country = $request->input('country');
+            $order->city = $request->input('city');
+            $order->zip = $request->input('zip');
+            
+            $order->save();
 
-        // Create new order items from products in the current order
-        foreach($request->input('orderItems') as $item){
-            $product = Product::find($item['product_id']);
+            // Create new order items from products in the current order
+            foreach($request->input('orderItems') as $item){
+                $product = Product::find($item['product_id']);
 
-            $orderItem = new OrderItem();
-            $orderItem->order_id = $order->id;
-            $orderItem->product_title = $product->title;
-            $orderItem->price = $product->price;
-            $orderItem->quantity = $item['quantity'];
-            $orderItem->ambassador_revenue = ($orderItem->price*$orderItem->quantity*0.1);
-            $orderItem->admin_revenue = ($orderItem->price*$orderItem->quantity*0.9);
+                $orderItem = new OrderItem();
+                $orderItem->order_id = $order->id;
+                $orderItem->product_title = $product->title;
+                $orderItem->price = $product->price;
+                $orderItem->quantity = $item['quantity'];
+                $orderItem->ambassador_revenue = ($orderItem->price*$orderItem->quantity*0.1);
+                $orderItem->admin_revenue = ($orderItem->price*$orderItem->quantity*0.9);
 
-            $orderItem->save();
+                $orderItem->save();
+            }
+
+            // Commit if everything ran OK
+            DB::commit();
+        }catch(Throwable $exception){
+            // Rollback on any exception
+            DB::rollBack();
+            // Return 400
+            abort(Response::HTTP_INTERNAL_SERVER_ERROR, "Data was not saved!");
         }
 
         return $order->load('orderItems');
