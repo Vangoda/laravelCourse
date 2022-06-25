2a7e4255e9ec919e1e408c259388590d6791fea4
Vangoda <vangoda@vangoda.com>
2022-06-25 15:05:24 +0000

    Added store function

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Link;
+use App\Models\LinkProduct;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class LinkController extends Controller
 {
@@ -11,5 +13,23 @@ class LinkController extends Controller
     {
         // Query Link models which match user id.
         return Link::whereUserId($id)->get();
+    }
+
+    public function store(Request $request){
+
+        $link = Link::create([
+            'user_id' => $request->user()->id,
+            'code' => Str::random(6)
+        ]);
+
+        foreach($request->input('products') as $productId){
+            // Create LinkProduct for each product
+            LinkProduct::create([
+                'product_id' => $productId,
+                'link_id' => $link->id
+            ]);
+        }
+
+        return $link;
     }
 }
