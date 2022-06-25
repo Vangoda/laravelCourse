6daf02719d4535b7153d46c50887bf441fdad263
Vangoda <vangoda@vangoda.com>
2022-06-25 13:50:39 +0000

    changed orders function to fetch only completed orders

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,12 +69,16 @@ class User extends Authenticatable
     // Attributes
 
     public function orders(){
-        return $this->hasMany(Order::class);
+        return $this->hasMany(Order::class)->where('complete', 1);
     }
 
     public function getRevenueAttribute(){
         return $this->orders->sum(
             fn(Order $order) => $order->ambassador_revenue
         );
+    }
+
+    public function getNameAttribute(){
+        return "$this->first_name $this->last_name";
     }
 }
