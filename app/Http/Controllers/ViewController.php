8d7ab119a5cbc093848b0f55555bb3a6464181b5
Vangoda <vangoda@vangoda.com>
2022-10-23 09:16:23 +0000

    Installed vue and added to laravel web route

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 class ViewController extends Controller
 {
     // Serve Vue App
-    public function app() { return view('vue-admin'); }
+    public function app()
+    {
+        return view('admin-app');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/{any}', 'ViewController@vue-admin')->where('any','^(?!api).*$');
+Route::any('/{any}', 'ViewController@admin-app')->where('any', '^(?!api).*$');
